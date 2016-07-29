95204add3c6fc583a449267c3305b5c2310f409b
Bo3o2S <honor2008@naver.com>
2016-07-29 09:28:48 +0900

    upload json

--- modules/QTtrojan.py
+++ modules/QTtrojan.py
@@ -4,7 +4,7 @@ from PyQt4.QtGui import *
 from PyQt4 import uic
 
 form_class = uic.loadUiType("trojan.ui")[0]
-print form_class
+
 
 class TrojanWindow(QMainWindow, form_class):
     def __init__(self):
@@ -14,7 +14,7 @@ class TrojanWindow(QMainWindow, form_class):
         self.connect(self.MakeTrojan_Button, SIGNAL("clicked()"), self.MakeTrojan_Clicked)
         self.connect(self.ExecuteTrojan_Button, SIGNAL("clicked()"), self.ExecuteTrojan_Clicked)
         self.clicked = 2
-        self.nonclicked = 0
+        self.noneclicked = 0
 
     def MakeTrojan_Clicked(self):
         Keylogger_Check = self.Keylogger_CheckBox.checkState()
@@ -22,7 +22,7 @@ class TrojanWindow(QMainWindow, form_class):
         TargetNum_Button = self.TargetNum_Button.toPlainText()
         if Keylogger_Check is self.clicked:
             MakeConfig(keylogger)
-        if ScreenShot_Check is self.nonclicked:
+        if ScreenShot_Check is self.noneclicked:
             MakeConfig(ScreenShot)
         self.MakeRandomDirectory(TargetNum_Button)
 
@@ -30,7 +30,7 @@ class TrojanWindow(QMainWindow, form_class):
     def ExecuteTrojan_Clicked(self):
         print 1
     def MakeRandomDirectory(self):
-
+        print 2
 if __name__ == "__main__":
     trojan = QApplication(sys.argv)
     trojanWindow = TrojanWindow()
